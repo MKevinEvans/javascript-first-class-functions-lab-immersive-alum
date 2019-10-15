369704d7fdb0dbb8c68f7160619f85d394f11b3b
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 18:47:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ function countdown(callback){
 }
 
 function createMultiplier(multiplyerValue){
-  switch(multiplyerValue)
+  switch(multiplyerValue){
+    case 2:
+      return function doubler(num){
+        return num*2
+      }
+    case 3:
+      return function tripler(num){
+        return num*3
+      }
+  }
   
 }
